9ed13b3088ed0c5735302a9a6fc9b5fa6c6d37a1
Andres Benitez <abolnaits@hotmail.com>
2020-11-10 11:23:03 -0500

    Componente Uno setState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 /*
 Parece HTML pero no lo es, esto es JSX
 JS con etiquetas especiales, Babel Repel es usado para esto
@@ -16,11 +16,47 @@ function App() {
   );
 }
 //Componente <Uno/>
+/*
 function Uno(props){
   console.log('Props ==> ',props);
   return(
   <div>Componente {props.myText}</div>
   );
+}
+*/
+class Uno extends React.Component{
+  //Objeto de estados
+  state = {
+    mostrar:true
+  }
+
+  mostrar = ()=>{
+    console.log('Mostrar ==>', this.state.mostrar);
+    //this.state.mostrar = false;
+    if(this.state.mostrar){
+      this.setState({mostrar:false});
+    }else{
+      this.setState({mostrar:true});
+    }
+    
+  }
+  //Metodo que devuelve el HTML
+  render(){
+    if(this.state.mostrar){
+      return(
+        <div>
+          Componente {this.props.myText}
+          <button onClick={this.mostrar}>Mostrar/Ocultar</button>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+            <button onClick={this.mostrar}>Mostrar/Ocultar</button>
+        </div>
+      )
+    }
+  }
 }
 //Exporto el componente
 export default App;
