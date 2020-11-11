7e8b4992f922ad3e1d784df06697e498ce0a342a
Andres Benitez <abolnaits@hotmail.com>
2020-11-11 12:51:30 -0500

    Componente FormaTarea state

--- src/componentes/FormaTarea.js
+++ src/componentes/FormaTarea.js
@@ -2,16 +2,35 @@ import React from 'react';
 
 class FormaTarea extends React.Component{
     
-    enviar(e){
+    state = {
+        titulo:'',
+        desc:''
+    }
+    
+    enviar = (e) => {
         e.preventDefault();
         console.log('Enviando...');
         //Obtengo los datos del Form
-
+        console.log(this.state);
     }
 
-    cambio(e){
+   
+    cambio = (e) => {
         console.log('Cambio...');
         console.log(e.target.value);
+        console.log(e.target.name);
+        /*
+        if(e.target.name == 'titulo'){
+            this.state.titulo = e.target.value;
+        }
+        if(e.target.name == 'desc'){
+            this.state.desc = e.target.value;
+        }
+        */
+       //Seteo el estado dependiendo del valor name del target
+       this.setState({
+           [e.target.name] : e.target.value
+       });
     }
     
     render(){
@@ -19,9 +38,9 @@ class FormaTarea extends React.Component{
             <div>
                 <h1>Agregar un tarea a la lista</h1>
             <form onSubmit={this.enviar}>
-                <input type="text" placeholder="Escribe una tarea" onChange={this.cambio}></input>
+                <input type="text" placeholder="Escribe una tarea" onChange={this.cambio} value={this.state.titulo} name="titulo"></input>
                 <br/>
-                <textarea placeholder="Descripcion de la tarea" onChange={this.cambio}></textarea>
+                <textarea placeholder="Descripcion de la tarea" onChange={this.cambio} value={this.state.desc} name="desc"></textarea>
                 <br/>
                 <input type="submit" value="Agregar" />
             </form>
